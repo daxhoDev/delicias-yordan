fcf01083d85056fba1d356523c29f5f2dbf8ba35
daxho <developer.daxho@gmail.com>
2025-09-11 15:32:58 -0400

    feat: footer

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,3 +1,22 @@
+import Image from "next/image";
+import brandLogoWhite from "@/public/brand-logo-white.png";
+
 export default function Footer(): React.ReactNode {
-  return <footer></footer>;
+  return (
+    <footer className="bg-grey-900 flex flex-col md:flex-row justify-center gap-12 items-center p-16">
+      <p className="text-grey-100 font-bold">
+        <span className="text-grey-500">2025</span>DELICIAS YORDAN
+      </p>
+      <Image
+        src={brandLogoWhite}
+        alt="Logo Delicias Yordan"
+        className="size-16"
+      />
+      <div className="flex items-center justify-center gap-4">
+        <span className="bg-grey-100 size-8"></span>
+        <span className="bg-grey-100 size-8"></span>
+        <span className="bg-grey-100 size-8"></span>
+      </div>
+    </footer>
+  );
 }
