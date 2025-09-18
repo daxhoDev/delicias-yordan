10a0c8a7e96455dac1e302f69ab385a3f63c92ab
daxho <developer.daxho@gmail.com>
2025-09-18 11:32:23 -0400

    fix: catalog flex direction

--- src/components/catalog/productsContainer.tsx
+++ src/components/catalog/productsContainer.tsx
@@ -34,7 +34,7 @@ export default function ProductsContainer({
   }
 
   return (
-    <main className="flex flex-col sm:flex-row flex-wrap items-center sm:items-stretch justify-center gap-x-16 gap-y-24">
+    <main className="flex flex-row flex-wrap items-center sm:items-stretch justify-center gap-x-16 gap-y-24">
       {catalog.map(
         (item) =>
           item.title.toLowerCase().includes(search.toLowerCase()) &&
