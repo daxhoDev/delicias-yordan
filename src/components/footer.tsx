b38cb759acf03c578a6850040b1650bc69fb7f75
daxho <developer.daxho@gmail.com>
2025-09-21 08:49:39 -0400

    feat: Facebook link onfooter

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,22 +1,33 @@
 import Image from "next/image";
+import Link from "next/link";
 import brandLogoWhite from "@/public/brand-logo-white.png";
+import Facebook from "@/src/components/icons/facebook";
 
 export default function Footer(): React.ReactNode {
   return (
-    <footer className="bg-grey-900 text-center flex z-10 flex-col md:flex-row justify-center gap-12 items-center px-16 py-32">
-      <p className="text-grey-100 font-bold">
-        <span className="text-grey-500">2025</span>DELICIAS YORDAN
-      </p>
-      <div className="size-16 aspect-square relative flex items-center justify-center">
-        <Image
-          src={brandLogoWhite}
-          width={499}
-          height={527}
-          alt="Logo Delicias Yordan"
-          className="size-16 "
-        />
+    <footer className="bg-grey-900 text-grey-100 text-center z-10 px-16 py-32">
+      <div className="flex flex-col md:flex-row justify-center gap-12 mb-12 items-center">
+        <p className="font-bold">
+          <span className="text-grey-500">2025</span>DELICIAS YORDAN
+        </p>
+        <div className="size-16 aspect-square relative flex items-center justify-center">
+          <Image
+            src={brandLogoWhite}
+            width={499}
+            height={527}
+            alt="Logo Delicias Yordan"
+            className="size-16 "
+          />
+        </div>
+        <Link href="https://www.facebook.com/ania.garces.hidalgo">
+          <Facebook
+            width={32}
+            height={32}
+            className="hover:text-grey-500 transition-all cursor-pointer"
+          />
+        </Link>
       </div>
-      <p className="text-grey-100">
+      <p>
         <span className="text-grey-300">Nos encontramos en</span> CALLE 6 #11 E/
         7 y 9, CIUDAD JARDÍN, HOLGUÍN, CUBA.
       </p>
